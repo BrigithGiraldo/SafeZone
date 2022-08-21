060099ba6474b1f0024c09f3b653069018d4e0b1
Dayana Arrieta Regino <72126244+DayanaArrieta@users.noreply.github.com>
2022-08-20 23:43:27 -0500

    commit

---
+++ cliente/src/App.js
@@ -0,0 +1,73 @@
+//paginas
+import Home from './paginas/Home';
+import Inicio from './paginas/Inicio';
+import Registro from './paginas/Registro';
+import Dashboard from './paginas/Dashboard';
+import Perfil from './paginas/Perfil';
+import Recomendados from './paginas/Recomendados';
+import Reportes from './paginas/Reportes';
+import About from './paginas/About';
+import Map from './paginas/map/Map';
+import Navbar from './componentes/Navbar';
+import Lugar from './paginas/Lugar';
+import Calificación from './paginas/Calificación';
+
+//Styled components
+import {StyledContainer} from './componentes/styles';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Router>
+      <StyledContainer>
+        <Switch>
+        <Route path='/Map'>
+        <Navbar />
+          <Map />
+          </Route>
+        <Route path='/About'>
+        <Navbar />
+          <About />
+          </Route>
+        <Route path='/Reportes'>
+        <Navbar />
+          <Reportes/>
+          </Route>
+        <Route path='/Recomendados'>
+        <Navbar />
+          <Recomendados />
+          </Route>
+        <Route path='/Perfil'>
+        <Navbar />
+            <Perfil />
+          </Route>
+        <Route path='/Lugar'>
+          <Navbar />
+            <Lugar />
+        </Route>
+        <Route path='/Calificación'>
+          <Navbar />
+            <Calificación />
+        </Route>
+          <Route path='/Registro'>
+            <Registro />
+          </Route>
+          <Route path='/Dashboard'>
+          <Navbar />
+          <Dashboard />
+          </Route>
+          <Route path='/Inicio'>
+            <Inicio />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </StyledContainer>
+    </Router>
+  );
+}
+
+export default App;
